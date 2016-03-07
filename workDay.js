834de3c4cf4aaf3202d6fbb4f7062bd2a55c7eaa
MikeZhang <mike.zhang@everbridge.com>
2016-03-07 11:14:56 +0800

    remove before files

--- workDay.js
+++
@@ -1,8 +0,0 @@
-define([], function() {
-    var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    return {
-        name: function(number) { return names[number]},
-        number: function(name) { return names.indexOf(name)}
-    }
-});
